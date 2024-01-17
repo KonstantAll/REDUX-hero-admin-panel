fcbd245941d5de170f3116916a319130b1a089b6
Kostya Alieksandrov <kostyuagusto@gmail.com>
2024-01-17 16:08:10 +0200

    hot fix

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,112 +1,103 @@
-// Задача для этого компонента:
-// Реализовать создание нового героя с введенными данными. Он должен попадать
-// в общее состояние и отображаться в списке + фильтроваться
-// Уникальный идентификатор персонажа можно сгенерировать через uiid
-// Усложненная задача:
-// Персонаж создается и в файле json при помощи метода POST
-// Дополнительно:
-// Элементы <option></option> желательно сформировать на базе
-// данных из фильтров
-
+import {useHttp} from '../../hooks/http.hook';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import {useDispatch, useSelector} from "react-redux";
-import {useHttp} from "../../hooks/http.hook";
-import {heroAdd} from "../../actions";
-import {useState} from "react";
+
+import { heroCreated } from '../../actions';
 
 const HeroesAddForm = () => {
+    // Состояния для контроля формы
+    const [heroName, setHeroName] = useState('');
+    const [heroDescr, setHeroDescr] = useState('');
+    const [heroElement, setHeroElement] = useState('');
 
-    const [hero, setHero] = useState({
-        id:uuidv4(),
-        name: '',
-        description: '',
-        element: '',
-    });
+    const {filters, filtersLoadingStatus} = useSelector(state => state.filters);
     const dispatch = useDispatch();
-    const {request} = useHttp()
+    const {request} = useHttp();
 
-    const {filters} = useSelector(state => state);
-
-    const clearForm = () => {
-        setHero({
-            id:uuidv4(),
-            name: '',
-            description: '',
-            element: '',
-        });
-    }
-
-    const onValueChange = (e) => {
-        setHero((hero) => ({...hero, [e.target.name]: e.target.value}))
-    }
-
-    const onSubmit = (e) => {
+    const onSubmitHandler = (e) => {
         e.preventDefault();
-        console.log('1111', hero)
+        // Можно сделать и одинаковые названия состояний,
+        // хотел показать вам чуть нагляднее
+        // Генерация id через библиотеку
+        const newHero = {
+            id: uuidv4(),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement
+        }
 
-        if(hero.name !== ''&&
-            hero.description !== ''&&
-            hero.element !== ''){
+        // Отправляем данные на сервер в формате JSON
+        // ТОЛЬКО если запрос успешен - отправляем персонажа в store
+        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
+            .then(res => console.log(res, 'Отправка успешна'))
+            .then(dispatch(heroCreated(newHero)))
+            .catch(err => console.log(err));
 
-            // const id = uuidv4();
-            // console.log('id', id)
-            // setHero((hero) => ({...hero, id}));
-            console.log('hero', hero);
-            request(`http://localhost:3001/heroes`, "POST", JSON.stringify(hero))
-                .then(res => console.log(res, 'Success'))
-                .then(dispatch(heroAdd(hero)))
-                .catch(err => console.log(err));
+        // Очищаем форму после отправки
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+    }
 
-            clearForm();
+    const renderFilters = (filters, status) => {
+        if (status === "loading") {
+            return <option>Загрузка элементов</option>
+        } else if (status === "error") {
+            return <option>Ошибка загрузки</option>
+        }
+        
+        // Если фильтры есть, то рендерим их
+        if (filters && filters.length > 0 ) {
+            return filters.map(({name, label}) => {
+                // Один из фильтров нам тут не нужен
+                // eslint-disable-next-line
+                if (name === 'all')  return;
+
+                return <option key={name} value={name}>{label}</option>
+            })
         }
     }
 
-
-    const content = filters.map(filter => {
-        return <option value={filter.name}>{filter.value}</option>
-    });
-
-
-
     return (
-        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmit}>
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input
-                    onChange={onValueChange}
-                    value={hero.name}
+                <input 
                     required
                     type="text" 
                     name="name" 
                     className="form-control" 
                     id="name" 
-                    placeholder="Как меня зовут?"/>
+                    placeholder="Как меня зовут?"
+                    value={heroName}
+                    onChange={(e) => setHeroName(e.target.value)}/>
             </div>
 
             <div className="mb-3">
                 <label htmlFor="text" className="form-label fs-4">Описание</label>
                 <textarea
-                    onChange={onValueChange}
-                    value={hero.description}
                     required
-                    name="description"
+                    name="text" 
                     className="form-control" 
                     id="text" 
                     placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
+                    style={{"height": '130px'}}
+                    value={heroDescr}
+                    onChange={(e) => setHeroDescr(e.target.value)}/>
             </div>
 
             <div className="mb-3">
                 <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select
-                    onChange={onValueChange}
-                    value={hero.element}
+                <select 
                     required
                     className="form-select" 
                     id="element" 
-                    name="element">
-                    <option>Я владею элементом...</option>
-                    {content}
+                    name="element"
+                    value={heroElement}
+                    onChange={(e) => setHeroElement(e.target.value)}>
+                    <option value="">Я владею элементом...</option>
+                    {renderFilters(filters, filtersLoadingStatus)}
                 </select>
             </div>
 
--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,10 +1,15 @@
 import {useHttp} from '../../hooks/http.hook';
-import {useCallback, useEffect} from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import {createSelector} from "reselect";
 
-import {heroesFetching, heroesFetched, heroesFetchingError, heroDelete} from '../../actions';
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
+
+import './heroesList.scss';
+
 
 // Задача для этого компонента:
 // При клике на "крестик" идет удаление персонажа из общего состояния
@@ -12,18 +17,22 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
+
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if(filter === 'all') {
+                return heroes
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        }
+    )
+    const filteredHeroes = useSelector(filteredHeroesSelector);
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
-
-    const onDelete = useCallback((key) => {
-        console.log('hello')
-        // dispatch(heroDelete(key));
-        request(`http://localhost:3001/heroes/${key}`, "DELETE")
-            .then(data => console.log(data, 'Deleted'))
-            .then(dispatch(heroDelete(key)))
-
-    }, [request]);
 
     useEffect(() => {
         dispatch(heroesFetching());
@@ -34,6 +43,18 @@ const HeroesList = () => {
         // eslint-disable-next-line
     }, []);
 
+    // Функция берет id и по нему удаляет ненужного персонажа из store
+    // ТОЛЬКО если запрос на удаление прошел успешно
+    // Отслеживайте цепочку действий actions => reducers
+    const onDelete = useCallback((id) => {
+        // Удаление персонажа по его id
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(data => console.log(data, 'Deleted'))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err));
+        // eslint-disable-next-line  
+    }, [request]);
+
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
     } else if (heroesLoadingStatus === "error") {
@@ -42,19 +63,32 @@ const HeroesList = () => {
 
     const renderHeroesList = (arr) => {
         if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
+            return (
+                <CSSTransition
+                    timeout={0}
+                    classNames="hero">
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            )
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem customKey={id} onDelete = {onDelete} {...props}/>
+            return (
+                <CSSTransition 
+                    key={id}
+                    timeout={500}
+                    classNames="hero">
+                    <HeroesListItem  {...props} onDelete={() => onDelete(id)}/>
+                </CSSTransition>
+            )
         })
     }
 
     const elements = renderHeroesList(filteredHeroes);
     return (
-        <ul>
+        <TransitionGroup component="ul">
             {elements}
-        </ul>
+        </TransitionGroup>
     )
 }
 
