dbce12ebf2212e276a086070204aca9f1c50b22d
Kostya Alieksandrov <kostyuagusto@gmail.com>
2024-02-05 16:35:57 +0200

    Include RTK Query in project. Remove heroes global state as it was.

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,10 +1,9 @@
-import {useHttp} from '../../hooks/http.hook';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
-import { heroCreated } from '../heroesList/heroesSlice';
 import {selectAll} from "../heroesFilters/filtersSlice";
+import {useCreateHeroMutation} from "../../api/apiSlice";
 import store from "../../store";
 
 const HeroesAddForm = () => {
@@ -13,10 +12,10 @@ const HeroesAddForm = () => {
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
+    const [createHero, {isLoading}] = useCreateHeroMutation();
+
     const {filtersLoadingStatus} = useSelector(state => state.filters);
     const filters = selectAll(store.getState());
-    const dispatch = useDispatch();
-    const {request} = useHttp();
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -32,11 +31,7 @@ const HeroesAddForm = () => {
 
         // Отправляем данные на сервер в формате JSON
         // ТОЛЬКО если запрос успешен - отправляем персонажа в store
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
-
+        createHero(newHero).unwrap()
         // Очищаем форму после отправки
         setHeroName('');
         setHeroDescr('');
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import heroes from "../components/heroesList/heroesSlice";
 import filters from "../components/heroesFilters/filtersSlice";
+import {apiSlice} from "../api/apiSlice";
 
-
+console.log('apiSlice',apiSlice)
 const stringMiddleWare = () => (next) => (action) => {
     if(typeof action === 'string') {
         return next({type: action});
@@ -10,23 +10,23 @@ const stringMiddleWare = () => (next) => (action) => {
     return next(action)
 }
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if(typeof action === 'string') {
-            return oldDispatch({type: action});
-        }
-        return oldDispatch(action)
-    }
-
-    return store
-}
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if(typeof action === 'string') {
+//             return oldDispatch({type: action});
+//         }
+//         return oldDispatch(action)
+//     }
+//
+//     return store
+// }
 
 const store = configureStore({
-    reducer: {heroes, filters},
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleWare),
+    reducer: {filters, [apiSlice.reducerPath]: apiSlice.reducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleWare, apiSlice.middleware),
     devTools: process.env.NODE_ENV !== 'production',
 
 })
